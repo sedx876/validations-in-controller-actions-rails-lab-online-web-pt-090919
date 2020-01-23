f1cd71f86564872bb9ce04a787b04767f1521c51
Sharon Watkins <swatkins876@gmail.com>
2020-01-23 21:14:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not valid please provide either Fiction or Non-Fiction" }
+  validates :content, length: { minimum: 100 }
 end
